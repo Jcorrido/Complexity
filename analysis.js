fd5d8973cbfb9c4b7fa1261a01e77232e1c7a416
Dylan James <djjames455@gmail.com>
2022-04-17 18:13:49 -0400

    Replace equality comparisons with strict versions

--- analysis.js
+++ analysis.js
@@ -6,7 +6,7 @@ function main()
 {
 	var args = process.argv.slice(2);
 
-	if( args.length == 0 )
+	if( args.length === 0 )
 	{
 		args = ["analysis.js"];
 	}
@@ -90,7 +90,7 @@ function traverseWithParents(object, visitor)
     for (key in object) {
         if (object.hasOwnProperty(key)) {
             child = object[key];
-            if (typeof child === 'object' && child !== null && key != 'parent') 
+            if (typeof child === 'object' && child !== null && key !== 'parent') 
             {
             	child.parent = object;
 					traverseWithParents(child, visitor);
@@ -165,7 +165,7 @@ function childrenLength(node)
 		if (node.hasOwnProperty(key)) 
 		{
 			child = node[key];
-			if (typeof child === 'object' && child !== null && key != 'parent') 
+			if (typeof child === 'object' && child !== null && key !== 'parent') 
 			{
 				count++;
 			}
@@ -178,8 +178,8 @@ function childrenLength(node)
 // Helper function for checking if a node is a "decision type node"
 function isDecision(node)
 {
-	if( node.type == 'IfStatement' || node.type == 'ForStatement' || node.type == 'WhileStatement' ||
-		 node.type == 'ForInStatement' || node.type == 'DoWhileStatement')
+	if( node.type === 'IfStatement' || node.type === 'ForStatement' || node.type === 'WhileStatement' ||
+		 node.type === 'ForInStatement' || node.type === 'DoWhileStatement')
 	{
 		return true;
 	}
@@ -201,7 +201,7 @@ if (!String.prototype.format) {
   String.prototype.format = function() {
     var args = arguments;
     return this.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined'
+      return typeof args[number] !== 'undefined'
         ? args[number]
         : match
       ;
@@ -216,7 +216,7 @@ function Crazy (argument)
 
 	var date_bits = element.value.match(/^(\d{4})\-(\d{1,2})\-(\d{1,2})$/);
 	var new_date = null;
-	if(date_bits && date_bits.length == 4 && parseInt(date_bits[2]) > 0 && parseInt(date_bits[3]) > 0)
+	if(date_bits && date_bits.length === 4 && parseInt(date_bits[2]) > 0 && parseInt(date_bits[3]) > 0)
     new_date = new Date(parseInt(date_bits[1]), parseInt(date_bits[2]) - 1, parseInt(date_bits[3]));
 
     var secs = bytes / 3500;
@@ -270,24 +270,24 @@ remainder.toString() + " seconds";
               for ( i = 0 ; i < cfield.value.length ; i++)
 				  {
 				    var n = cfield.value.substr(i,1);
-				    if ( n != 'a' && n != 'b' && n != 'c' && n != 'd'
-				      && n != 'e' && n != 'f' && n != 'g' && n != 'h'
-				      && n != 'i' && n != 'j' && n != 'k' && n != 'l'
-				      && n != 'm' && n != 'n' && n != 'o' && n != 'p'
-				      && n != 'q' && n != 'r' && n != 's' && n != 't'
-				      && n != 'u' && n != 'v' && n != 'w' && n != 'x'
-				      && n != 'y' && n != 'z'
-				      && n != 'A' && n != 'B' && n != 'C' && n != 'D'
-				      && n != 'E' && n != 'F' && n != 'G' && n != 'H'
-				      && n != 'I' && n != 'J' && n != 'K' && n != 'L'
-				      && n != 'M' && n != 'N' &&  n != 'O' && n != 'P'
-				      && n != 'Q' && n != 'R' && n != 'S' && n != 'T'
-				      && n != 'U' && n != 'V' && n != 'W' && n != 'X'
-				      && n != 'Y' && n != 'Z'
-				      && n != '0' && n != '1' && n != '2' && n != '3'
-				      && n != '4' && n != '5' && n != '6' && n != '7'
-				      && n != '8' && n != '9'
-				      && n != '_' && n != '@' && n != '-' && n != '.' )
+				    if ( n !== 'a' && n !== 'b' && n !== 'c' && n !== 'd'
+				      && n !== 'e' && n !== 'f' && n !== 'g' && n !== 'h'
+				      && n !== 'i' && n !== 'j' && n !== 'k' && n !== 'l'
+				      && n !== 'm' && n !== 'n' && n !== 'o' && n !== 'p'
+				      && n !== 'q' && n !== 'r' && n !== 's' && n !== 't'
+				      && n !== 'u' && n !== 'v' && n !== 'w' && n !== 'x'
+				      && n !== 'y' && n !== 'z'
+				      && n !== 'A' && n !== 'B' && n !== 'C' && n !== 'D'
+				      && n !== 'E' && n !== 'F' && n !== 'G' && n !== 'H'
+				      && n !== 'I' && n !== 'J' && n !== 'K' && n !== 'L'
+				      && n !== 'M' && n !== 'N' &&  n !== 'O' && n !== 'P'
+				      && n !== 'Q' && n !== 'R' && n !== 'S' && n !== 'T'
+				      && n !== 'U' && n !== 'V' && n !== 'W' && n !== 'X'
+				      && n !== 'Y' && n !== 'Z'
+				      && n !== '0' && n !== '1' && n !== '2' && n !== '3'
+				      && n !== '4' && n !== '5' && n !== '6' && n !== '7'
+				      && n !== '8' && n !== '9'
+				      && n !== '_' && n !== '@' && n !== '-' && n !== '.' )
 				    {
 				      window.alert("Only Alphanumeric are allowed.\nPlease re-enter the value.");
 				      cfield.value = '⠀';
